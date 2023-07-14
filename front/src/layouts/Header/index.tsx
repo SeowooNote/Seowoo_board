e64899d527b37d1b3052e04e0a81d0eb2eaeec8c
seowoo <dkr0210@naver.com>
2023-07-14 18:21:06 +0900

    Project 4일차

--- front/src/layouts/Header/index.tsx
+++ front/src/layouts/Header/index.tsx
@@ -1,22 +1,45 @@
 import React, { useState } from 'react';
 import './style.css';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Header() {
   const [serachState, setSerachState] = useState<boolean>(false);
+  const [login, setLogin] = useState<boolean>(true);
 
+  const navigator = useNavigate();
   const { pathname } = useLocation();
+
+  const showSearch = pathname !== '/my-page' && pathname !== '/board/write' && pathname.indexOf('/board/update') === -1;
+  const isAuth = pathname === '/authentication';
+  const isMyPage = pathname === '/my-page';
+  const showUpload = pathname === '/board/write' || pathname.indexOf('/board/update') !== -1;
+
+  const onLogoClickHandler = () => {
+    navigator('/');
+  }
+  
+  const onSignInButtonClickHandler = () => {
+    setLogin(true);
+    navigator('/authentication');
+  }
+
+  const onMyPageButtonClickHandler = () => {
+    navigator('/my-page');
+  }
+
+  const onSignOutButtonClickHandler = () => {
+    setLogin(false);
+    navigator('/');
+  }
 
   return (
     <div id='header'>
-      <div className="header-left">
+      <div className="header-left" onClick={onLogoClickHandler}>
         <div className="header-left-logo-icon"></div>
         <div className="header-left-logo-text">Seowoo's Board</div>
       </div>
       <div className="header-right">
-        { (pathname !== '/my-page' && pathname !== '/board/write' && pathname !== '/board/update') &&
-          (serachState ?
-            (
+        { (showSearch) && (serachState ? (
               <div className="header-search-box">
                 <input className='header-search-input' type="text" />
                 <div className="header-icon-box" onClick={() => setSerachState(false)}>
@@ -30,13 +53,11 @@ export default function Header() {
             ) )
         }
         {
-          pathname !== '/authentication' && (
-            <>
-              <div className="header-black-button">로그인</div>
-              <div className="header-white-button">마이페이지</div>
-              <div className="header-white-button">로그아웃</div>
-              <div className="header-black-disable-button">업로드</div>
-            </>
+          !isAuth && (
+            isMyPage ? (<div className="header-white-button" onClick={onSignOutButtonClickHandler}>로그아웃</div>) :
+            showUpload ? (<div className="header-black-disable-button">업로드</div>) :
+            login ? (<div className="header-white-button" onClick={onMyPageButtonClickHandler}>마이페이지</div>) : 
+                    (<div className="header-black-button" onClick={onSignInButtonClickHandler}>로그인</div>)
           )
         }
       </div>
--- front/src/views/Authentication/index.tsx
+++ front/src/views/Authentication/index.tsx
@@ -1,8 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
+import InputBox from 'src/components/InputBox';
+import { INPUT_ICON } from 'src/constants';
 
 export default function Authentication() {
+  const [view, setView] = useState<'sign-in' | 'sign-up'>('sign-in');
+
+  const SignInCard = () => {
+
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [error, setError] = useState<boolean>(false);
+
+    const onPasswordIconClickHandler = () => {
+      setShowPassword(!setShowPassword);
+    }
+
+    const onSignUpClickHandler = () => {
+      setView('sign-up');
+    }
+
+    return(
+      <div className="authentication-card">
+        <div className="authentication-card-top">
+          <div className="authentication-card-top-text-container">
+            <div className="authentication-card-top-text">로그인</div>
+          </div>
+          <div className="authentication-card-top-input-container">
+            <InputBox label='이메일 주소' type='text' placeholder='이메일 주소를 입력해주세요' error = {error}/>
+            <InputBox label='비밀번호' type={showPassword ? 'text' : 'password'} placeholder='비밀번호를 입력해주세요' icon={showPassword ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordIconClickHandler} error={error}/>
+          </div>
+        </div>
+        <div className="authentication-card-bottom">
+          { error && (
+                      <div className='authentication-card-bottom-error-message'>   
+                        {`이메일 주소 또는 비밀번호를 잘못 입력했습니다.\n입력하신 내용을 다시 확인해주세요.`}
+                      </div>
+          ) }
+          <div className="authentication-card-bottom-button">로그인</div>
+          <div className="authentication-card-bottom-text">
+            신규 사용자이신가요? <span className="authentication-emphasis" onClick={onSignUpClickHandler}>회원가입</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  const SignUpCard = () => {
+    const [page, setPage] = useState<1 | 2>(1);
+    const [showPassword, setPassword] = useState<boolean>(false);
+    const [showPasswordCheck, setPasswordCheck] = useState<boolean>(false);
+
+    const [emailPatternError, setEmailPatternError] = useState<boolean>(false);
+    const [emailDuplicationError, setEmailDuplicationError] = useState<boolean>(false);
+    const [passwordError, setPasswordError] = useState<boolean>(false);
+    const [passwordCheckError, setPasswordCheckError] =useState<boolean>(false);
+
+    const [nicknameError, setNickNameError] = useState<boolean>(false);
+    const [telNumberError, setTelNumberError] = useState<boolean>(false);
+    const [addressError, setAddressError] = useState<boolean>(false);
+
+    const onPasswordIconClickHandler = () => {
+      setPassword(!setPassword);
+    }
+
+    const onPasswordCheckIconClickHandler = () => {
+      setPasswordCheck(!setPasswordCheck);
+    }
+
+    const onButtonClickHandler = () => {
+      if (page === 1) setPage(2);
+      else setPage(1);
+    }
+
+    const onSignInClickHandler = () => {
+      setView('sign-in');
+    }
+
+    return(
+      <div className="authentication-card">
+        <div className="authentication-card-top">
+          <div className="authentication-card-top-text-container">
+            <div className="authentication-card-top-text">회원가입</div>
+            <div className="authentication-card-top-text-opacity">{`${page}/2`}</div>
+          </div>
+          <div className="authentication-card-top-input-container">
+            {
+              page === 1 ? (
+                              <>
+                                <InputBox label='이메일 주소*' type='text' placeholder='이메일 주소를 입력해주세요' error={emailPatternError || emailDuplicationError} helper={emailPatternError ? '이메일 주소 포맷이 맞지 않습니다.' : emailDuplicationError ? '중복되는 이메일 주소 입니다.' : ''} />
+                                <InputBox label='비밀번호*' type={showPassword ? 'text' : 'password'} placeholder='비밀번호를 입력해주세요' icon={showPassword ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordIconClickHandler} error={passwordError} helper={passwordError ? '비밀번호는 8자 이상 입력해주세요.' : ''} />
+                                <InputBox label='비밀번호 확인*' type={showPasswordCheck ? 'text' : 'password'} placeholder='비밀번호를 다시 입력해주세요' icon={showPasswordCheck ? INPUT_ICON.ON : INPUT_ICON.OFF} buttonHandler={onPasswordCheckIconClickHandler} error={passwordCheckError} helper={passwordCheckError ? '비밀번호가 일치하지 않습니다.' : ''} />
+                              </>
+                            ) : (
+                              <>
+                                <InputBox label='닉네임*' type='text' placeholder='넥네임을 입력해주세요' error={nicknameError} helper={nicknameError ? '닉네임을 입력해주세요.' : ''} />
+                                <InputBox label='핸드폰 번호*' type='text' placeholder='해드폰 번호를 입력해주세요' error={telNumberError} helper={telNumberError ? '숫자만 입력해주세요.' : ''} />
+                                <InputBox label='주소*' type='text' placeholder='우편번호 찾기' icon={INPUT_ICON.ARROW} error={addressError} helper={addressError ? '우편번호를 선택해주세요.' : ''} />
+                                <InputBox label='상세 주소*' type='text' placeholder='상세 주소를 입력해주세요' />
+                              </>)
+            }
+          </div>
+        </div>
+        <div className="authentication-card-bottom">
+          <div className="authentication-card-bottom-button" onClick={onButtonClickHandler}>
+            { page === 1 ? '다음 단계' : '회원가입' }
+          </div>
+          <div className="authentication-card-bottom-text">이미 계정이 있으신가요? 
+            <span className='authentication-emphasis-login' onClick={onSignInClickHandler}> 로그인</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div>Authentication</div>
-  )
+    <div id='authentication-wrapper'>
+      <div className='authentication-left'>
+        <div className="authentication-left-icon"></div>
+        <div className="authentication-left-text-container">
+          <div className='authentication-left-text'>환영합니다.</div>
+          <div className='authentication-left-text'>Seowoo's Board 입니다.</div>
+        </div>
+      </div>
+      <div className='authentication-right'>
+        {view === 'sign-in' ? (<SignInCard />) : (<SignUpCard />)}
+      </div>
+    </div>
+  );
 }
